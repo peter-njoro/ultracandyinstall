76a56094f61358a9b788db6b0f65ec08e6b5b34b
CANDY <mirukaiano@gmail.com>
2025-10-19 16:22:10 +0300

    Update Update_UltraCandy.sh

--- Update_UltraCandy.sh
+++ Update_UltraCandy.sh
@@ -969,19 +969,19 @@ setup_ultracandy() {
         print_status "Ensuring necessary packages are installed"
         echo
         $AUR_HELPER --noconfirm -S waybar waypaper-git swaync hyprviz-bin meson cpio cmake
-        hyprpm enable hyprbars
     else
         print_status "Ensuring necessary packages are installed"
         echo
         $AUR_HELPER --noconfirm -S ags-hyprpanel-git mako hyprviz-bin meson cpio cmake
-        hyprpm enable hyprbars
     fi
 
     print_status "Setting up hyprexpo-plus for wrokspace overview..."
 
     hyprpm update
+    hyprpm add https://github.com/hyprwm/hyprland-plugins
     hyprpm add https://github.com/sandwichfarm/hyprexpo-plus
     hyprpm update
+    hyprpm enable hyprbars
     hyprpm enable hyprexpo-plus
     hyprpm disable hyprexpo
     hyprpm reload
